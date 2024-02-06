6a2274848cf87613cfe26e8accc756090db00be7
BekirovaMavile <94888656+BekirovaMavile@users.noreply.github.com>
2024-02-06 11:44:57 +0300

    Анимация

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:jewellry_shop/states/jew_state.dart';
 import 'package:jewellry_shop/ui/_ui.dart';
 import 'package:jewellry_shop/ui/screens/home_screen.dart';
-import 'package:jewellry_shop/ui/screens/splash_screen.dart';
 import 'package:jewellry_shop/ui_kit/app_theme.dart';
 
 
